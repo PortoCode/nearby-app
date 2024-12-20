365e8f41f51aec66cdddab6eb66564841c563f27
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2024-12-19 22:57:28 -0300

    Add `HomeView`

---
+++ NearbyApp/Sources/Features/Models/CategoryModel.swift
@@ -0,0 +1,11 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2024 PortoCode. All Rights Reserved.
+//
+
+import Foundation
+
+struct Category: Decodable {
+    let id: String
+    let name: String
+}
---
+++ NearbyApp/Sources/Features/Models/PlaceAnnotation.swift
@@ -0,0 +1,19 @@
+//
+// Created by Rodrigo Porto.
+// Copyright © 2024 PortoCode. All Rights Reserved.
+//
+
+import Foundation
+import MapKit
+
+class PlaceAnnotation: NSObject, MKAnnotation {
+    let title: String?
+    let subtitle: String?
+    let coordinate: CLLocationCoordinate2D
+    
+    init(place: Place) {
+        self.title = place.name
+        self.subtitle = place.description
+        self.coordinate = CLLocationCoordinate2D(latitude: place.latitude, longitude: place.longitude)
+    }
+}
